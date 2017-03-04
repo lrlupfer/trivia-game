6e88d7b2a7098b99bac0650be74e5a1fe5d845fd
Lindsey Lupfer <lindsey@Lindseys-MacBook-Pro.local>
2017-03-03 23:09:28 -0500

    first push, html and css mostly done, js not working

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	// declaring variables
+	var time = 20,
+		correct = 0,
+		incorrect = 0,
+		current = 0,
+		trivia = [],
+		timer,
+		display,
+		i,
+		randomizedArray,
+		game,
+
+	// click to start the game
+	$('#start').click(function() {
+		alert( "wow this finally worked!" );
+	});
+
+	// click to reset the game
+});
